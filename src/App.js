7b31169411c2764cc9d42425c4cfd68fdd8cc05d
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-28 20:48:40 +0530

    started All courses component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Heading from "./components/Heading";
 import Programs from "./components/Programs";
+import AllCourses from "./components/AllCourses";
 
 function App() {
   return  (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Heading />
       <Programs />
+      <AllCourses />
     </>
   );
 }
---
+++ src/components/AllCourses.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllCourses() {
+  return (
+    <div className='ml-24 mt-20'>
+        <h1 className='text-4xl font-bold'>All Courses</h1>
+    </div>
+  )
+}
+
+export default AllCourses
