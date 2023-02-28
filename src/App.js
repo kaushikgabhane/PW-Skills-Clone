2e5adc5f2f4b87face62338dcfd880c648d9ab2b
kaushikgabhane <kaushikgabhane705@gmail.com>
2023-02-28 19:04:54 +0530

    created programs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Heading from "./components/Heading";
+import Programs from "./components/Programs";
 
 function App() {
   return  (
     <>
       <Navbar />
       <Heading />
+      <Programs />
     </>
   );
 }
